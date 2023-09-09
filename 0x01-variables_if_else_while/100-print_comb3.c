d602cb62a81b3ee7c3adf8b69ed026ff40edb3f7
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-08 20:16:41 -0700

    hh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - view
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 0; x < 9; x++)
+	{
+for (y = x + 1; y < 10; y++)
+	{
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
+
+if (x == 8 && y == 9)
+putchar(',');
+}
+}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,11 @@ int main(void)
 			{
 				putchar((x / 10) + '0');
 				putchar((x % 10) + '0');
-				putchar('\0');
 				putchar((y / 10) + '0');
 				putchar((y % 10) + '0');
 				if (x == 98 && y == 99)
 					continue;
 				putchar('.');
-				putchar ('\0');
 			}
 		}
 	putchar('\n');
