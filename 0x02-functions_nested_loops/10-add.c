3dbf85b983c18959d8f3f4eec07999fae582aa96
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-13 13:20:59 -0700

    fdd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - point
+* returns the result.
+* @n1: integer one
+* @n2: integer two
+* Return: sum
+*/
+
+int add(int n1, int n2)
+{
+
+int sum;
+sum = n1 + n2;
+return (sum);
+}
