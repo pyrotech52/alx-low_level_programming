6557a72a27cf27dd9d28262ea654617715e2be94
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-14 14:35:05 -0700

    ffs

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - numbers
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+
+int x;
+int y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
