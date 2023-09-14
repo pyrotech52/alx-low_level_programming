57a8199e58bf111dba4e52b448dd49f1a9ed9500
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-14 13:39:10 -0700

    fff

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit - chech number
+* @c: parameter
+* Return: 1 if digit
+*/
+
+int _isdigit(int c)
+{
+
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplication
+* @a: parameters
+* @b: parameters
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
