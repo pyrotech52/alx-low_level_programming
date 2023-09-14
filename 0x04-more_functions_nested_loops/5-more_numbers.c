023e59067d33e2060ca037f15c3239e5a04e9969
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-14 14:47:53 -0700

    ff

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,11 @@ for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
 {
-_putchar(y + '0');
+
+if (y > 9)
+_putchar(y / 10 + '0');
+_putchar(y % 10 + '0');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
