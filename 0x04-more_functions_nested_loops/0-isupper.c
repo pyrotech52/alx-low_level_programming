e13a101cc8823eb2a8066f2668a921e539a469b4
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-14 13:08:48 -0700

    fdsf

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - uppercase
+* @c: parameter
+* Return: 1 for uppercase
+*/
+
+int _isupper(int c)
+{
+
+if (c > 'A' && c < 'Z')
+return (1);
+else
+return (0);
+}
