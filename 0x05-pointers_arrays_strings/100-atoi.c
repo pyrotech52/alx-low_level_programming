1678f4f0f988e3d6ddaefe9a090725ae62a2597f
pyrotech52 <coke.pyrotech@gmail.com>
2023-09-18 20:15:50 -0700

    ddd

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts
+ * @s: parameter
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int a, b, c, d, e, f;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	e = 0;
+	f = 0;
+
+	while (s[d] != '\0')
+	d++;
+
+	while (a < d && e == 0)
+	{
+	if (s[a] == '-')
+	++b;
+
+	if (s[a] >= '0' && s[a] <= '9')
+	{
+	f = s[a] - '0';
+	if (b % 2)
+	f = -f;
+	n = n * 10 + f;
+	e = 1;
+	if (s[a + 1] < '0' || s[a + 1] > '9')
+	break;
+	e = 0;
+	}
+	a++;
+	}
+
+	if (e == 0)
+	return (0);
+
+	return (n);
+}
+
