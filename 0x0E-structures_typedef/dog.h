4cafbb86aa8a322718aa8e8b0ed12fa5f7af2746
pyrotech52 <coke.pyrotech@gmail.com>
2023-10-10 17:56:03 -0700

    esf

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,9 @@ void print_dog(struct dog *d);
  * dog_t - new dog type
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif
