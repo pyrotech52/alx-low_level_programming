40ba8d68abde0c6962abbb203a8ee25fa5ce6ec4
pyrotech52 <coke.pyrotech@gmail.com>
2023-10-10 17:48:55 -0700

    ddd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - new dog type
+ */
+typedef struct dog dog_t;
 
 #endif
