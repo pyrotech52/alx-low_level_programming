2295d8bcbc18409e3be69f2bb57163865114ea22
pyrotech52 <coke.pyrotech@gmail.com>
2023-10-26 18:37:26 -0700

    ddd

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,10 +7,8 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index > sizeof(n) * 8)
 return (-1);
-}
-*n &= ~(1UL << index);
-return (0);
+*n &= ~(1 << index);
+return (1);
 }
